5adee97d09764bc60a52dd6253de9e6e77559b5b
Olivier <olivier.bent@gmail.com>
2023-03-10 12:31:27 +0100

    creation components optimisation pages views

---
+++ frontend-vtt/ecommerce-vtt/src/components/ProductsList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-if="products.length > 0">
+    <ProductsListItem
+      v-for="product in products"
+      :key="product.id"
+      :product="product"
+    />
+  </div>
+  <p v-else>Il n'y a aucun produit dans votre panier</p>
+</template>
+
+<script>
+import ProductsListItem from "./ProductsListItem.vue";
+
+export default {
+  name: "ProductsList",
+  props: ["products"],
+  components: {
+    ProductsListItem,
+  },
+};
+</script>
