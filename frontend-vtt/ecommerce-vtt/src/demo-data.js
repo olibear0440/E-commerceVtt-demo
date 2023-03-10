e7752aa6c9a9da41ea8d95e907ed3ee394e50b80
Olivier <olivier.bent@gmail.com>
2023-03-10 12:20:55 +0100

    rajout img + fichier data des vtt

---
+++ frontend-vtt/ecommerce-vtt/src/demo-data.js
@@ -0,0 +1,127 @@
+import vttImg1 from "./assets/img/vttBmc.jpg";
+import vttImg2 from "./assets/img/vttcannondale.jpg";
+import vttImg3 from "./assets/img/vttcubestereo.jpg";
+import vttImg4 from "./assets/img/vttfocus.jpg";
+import vttImg5 from "./assets/img/vttgiant.jpg";
+import vttImg6 from "./assets/img/vttgravity.jpg";
+import vttImg7 from "./assets/img/vttlapierre.jpg";
+import vttImg8 from "./assets/img/vttmoustache.jpg";
+import vttImg9 from "./assets/img/vttorbea.jpg";
+import vttImg10 from "./assets/img/vttpivot.jpg";
+//import vttImg11 from "./assets/img/vttscott.jpg";
+//import vttImg12 from "./assets/img/vtttrek.jpg";
+
+export const products = [
+  {
+    id: "1",
+    name: " VTT Twostroke AL FIVE",
+    price: "1399",
+    description:
+      "Transmission Shimano Deore 11 vitesses. Fourche SR SUNTOUR XCM 32. Roues XCD23. Champion de VTT, Julien Absalon, a emmené son team Absolute Absalon sur la plus haute marche du podium des championnats de monde 2020 grâce au VTT BMC Fourstroke.",
+    imageUrl: vttImg1,
+    averageRating: "5.0",
+  },
+  {
+    id: "2",
+    name: "VTT CANNONDALE F-SI ",
+    price: "2563",
+    description:
+      "VTT cross-country géométrie spécifique. Technologies Micro-suspension Save sur le F-si et Flex-pivot sur le Scalpel. Electronique connectée et embarquée.",
+    imageUrl: vttImg2,
+    averageRating: "5.0",
+  },
+  {
+    id: "3",
+    name: "VTT CUBE STEREO HYBRID ",
+    price: "9102",
+    description:
+      "cadres système SizeSplit, batterie de 750W Bosch CX quatrième génération. Axe arrière Boost 148, ses larges roulements de pivots et son All Mountain Geometry offrent un pilotage précis. Patte de dérailleur Sram UDH pour une protection contre les chocs en cas d'accidents.",
+    imageUrl: vttImg3,
+    averageRating: "5.0",
+  },
+  {
+    id: "4",
+    name: "VTT FOCUS RAVEN",
+    price: "3199",
+    description:
+      "Cadre en carbone léger, axes traversants RAT et confort CIA. Fourche suspendu ROCKSHOX SID SELECT légère avec 100 mm de débattement. Groupe SRAM GX EAGLE 52, roues rapides DT SWISS X1900 et tige de selle en carbone.",
+    imageUrl: vttImg4,
+    averageRating: "5.0",
+  },
+  {
+    id: "5",
+    name: "VTT GIANT TRANCE X ELEC",
+    price: "6400",
+    description:
+      "Moteur SyncDrive Pro délivrant 85Nm de couple avec un ratio d'assistance personnalisable jusqu'à 400%. Suspension Giant Maestro optimisée pour une géométrie 29 pouces avec 140mm/150mm de débattement arrière/avant. Technologie Giant Flip-Chip directement compatible avec la batterie EnergyPak Smart 800.",
+    imageUrl: vttImg5,
+    averageRating: "5.0",
+  },
+  {
+    id: "6",
+    name: "VTT GT GRAVITY",
+    price: "3499",
+    description:
+      "VTTAe de 150 mm de débattement à l'avant comme à l'arrière. Le cadre est en alu et les roues en 29 pouces. C'est un bike destiné à un programme all-mountain, équipé d'une motorisation shimano. Transmission Sram Eagle SX 11/50 12v. Pneus Maxxis Minion DHF/DHRII 2.6 3C Exo+. Amortisseur Rockshox Deluxe Select R.",
+    imageUrl: vttImg6,
+    averageRating: "5.0",
+  },
+  {
+    id: "7",
+    name: "VTT LAPIERRE ZESTY TR",
+    price: "2099",
+    description:
+      "Amortisseur Lapierre AF2 Air Trunnion Metric Rebound & Lockout (165*45). Boîtier de pédalier Shimano BBMT800 Pressfit. Cadre Zesty TR Supreme 5 alloy 120mm Travel (29″) Pressfit Boost, Metric, Thru Axle. Cassette New Shimano Deore Cs- M5100-11 Spd 11-51T.",
+    imageUrl: vttImg7,
+    averageRating: "5.0",
+  },
+  {
+    id: "8",
+    name: "VTT MOUSTACHE SAMEDI 27",
+    price: "4199",
+    description:
+      "deux versions de cadre, standard et Open. Moteur Active Plus de .Performance Line et ses 65Nm. 3 moteurs jantes spécifiques Moustache et pneus Maxxis.",
+    imageUrl: vttImg8,
+    averageRating: "5.0",
+  },
+  {
+    id: "9",
+    name: "VTT ORBEA RISE E-BIKE",
+    price: "5299",
+    description:
+      "Partenariat avec Shimano, moteur EP8. Batterie modulaire qui combine une batterie principale très légère de 360Wh à une batterie optionnelle de 252Wh.",
+    imageUrl: vttImg9,
+    averageRating: "5.0",
+  },
+  {
+    id: "10",
+    name: "VTT PIVOT MACH 4 SL",
+    price: "7199",
+    description:
+      "Tout-suspendu de Cross-Country 29 pouces, cadre du Mach 4 SL s'affiche comme l'un des plus légers du marché, manchons durs en EPS tapissés. Standard Boost (110/148 mm)",
+    imageUrl: vttImg10,
+    averageRating: "5.0",
+  },
+  /*
+  {
+    id: "11",
+    name: "VTT SCOTT RANSOM",
+    price: "3999",
+    description:
+      "Le 930 est équipé d'une fourche RockShox Domain, de notre système de suspension TwinLoc, d'une transmission Shimano à 12 vitesses et de composants Syncros. Cadre Ransom Alloy SL 6011 Custom butted / VLK Virtual 4 Link Kinematic.",
+    imageUrl: vttImg11,
+    averageRating: "5.0",
+  },
+  {
+    id: "12",
+    name: "VTT TREK SUPERCALIBER",
+    price: "5299",
+    description:
+      "Fourche RockShox Reba RL, ressort Solo Air, amortisseur Motion Control, blocage au guidon, pivot conique, déport de 42 mm, Boost110, Maxle Stealth 15 mm, débattement de 100 mm. Derailleur arrière. Shimano XT M8100, longue chape, pignon 51 dts max.",
+    imageUrl: vttImg12,
+    averageRating: "5.0",
+  },
+  */
+];
+
+export const cartItems = [products[0], products[2], products[3]];
