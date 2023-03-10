8f76071db6160bfea2981fbc955966f4b9f4a344
Olivier <olivier.bent@gmail.com>
2023-03-10 12:11:09 +0100

    install express app.js + test requete ok

--- backend-vtt/server.js
+++ backend-vtt/server.js
@@ -1,8 +1,9 @@
 const http = require("http");
+const app = require('./app');
 
 
-const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur');
-});
+
+app.set('port', 3000);
+const server = http.createServer(app);
 
 server.listen(3000);
